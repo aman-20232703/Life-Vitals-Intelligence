6d38c42b9448a87249ca6b55a54f45d21401549b
Aman Kumar <168708176+aman-20232703@users.noreply.github.com>
2026-05-04 11:03:42 +0530

    Switch to libSQL client for database connection
    Replaced SQLite database connection with libSQL client connection.

--- stitch/backend/database.py
+++ stitch/backend/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./lifevitals.db"
+from libsql_client import create_client
+
+client = create_client(
+    url="libsql://lifevital-aman2703.aws-ap-south-1.turso.io",
+    auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3Nzc4NzI2NzEsImlkIjoiMDE5ZGYxNzctOTkwMS03YmUzLTlmMTAtOWEzZjBjZjlhNmUzIiwicmlkIjoiMjllMDhkNGMtYzU3Mi00ZDFiLTkxNDgtY2E1OWI5ZGNjNDRhIn0.BYO-FwJh41Q0juxZRFtQ-sJTsBiULtcpSddcpbfmFY7mi8GEyTbN7mRgg7R000QJi4kOWZhwGVQryEtgPBvzCw"
+)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
